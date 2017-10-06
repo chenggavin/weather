27e5334e22af9201b0f877475c447f8fb5b28895
Gavin Cheng <gavincheng01@gmail.com>
2017-10-06 11:32:40 -0400

    hello

--- app/main.js
+++ app/main.js
@@ -27,7 +27,7 @@ document.onreadystatechange = function() {
 
 function getWeather() {
 
-  var url="http://api.openweathermap.org/data/2.5/weather?zip=<zipcode>&us&appid=ef6a94dab254dc386b931af4d5ca58c7";
+  var url="https://api.openweathermap.org/data/2.5/weather?zip=<zipcode>&us&appid=ef6a94dab254dc386b931af4d5ca58c7";
   url = url.replace("<zipcode>", zipInput.value); 
 
   apiRequest = new XMLHttpRequest();
